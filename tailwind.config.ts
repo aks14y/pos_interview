4f08f69d02e312eeaa62af05472ab0bad6a871a9
aks14y <akshay4362847@gmail.com>
2025-02-01 16:50:55 +0530

    UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "custom-yellow": "#f6f6ef",
+        "level-0": "#ecfff1",
+        "level-1": "#f8f8a7",
+        "level-2": "#a6d96a",
+        "level-3": "#1a9641",
+        "level-4": "#003f0b",
+      },
     },
   },
   plugins: [],
