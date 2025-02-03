56aac8f98f5af809043f09f75b3f7ed159fbf41e
aks14y <akshay4362847@gmail.com>
2025-02-03 15:14:06 +0530

    fix: lint

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,5 +1,4 @@
-export default function Sidebar({candidateData, onCandidateClick}) {
-
+export default function Sidebar({ candidateData, onCandidateClick }) {
   return (
     <div className="w-80 bg-white border-r border-gray-200 border-2">
       <h2 className="text-lg font-semibold text-gray-800 mb-4 border-b-2  p-3">
@@ -7,21 +6,29 @@ export default function Sidebar({candidateData, onCandidateClick}) {
       </h2>
       <div className="p-3 text-custom-yellow border-b-2">
         <ul className="space-y-2 ">
-          {...candidateData.slice(0,3).map((candidate, index) => (
-            <li key={index} onClick={() => onCandidateClick(candidate.id)}  className="text-gray-700 border-b-2 pb-1">
+          {...candidateData.slice(0, 3).map((candidate, index) => (
+            <li
+              key={index}
+              onClick={() => onCandidateClick(candidate.id)}
+              className="text-gray-700 border-b-2 pb-1"
+            >
               {candidate.name}
             </li>
           ))}
         </ul>
         <p className="mt-4 text-sm text-gray-500">
-          Recommendations are based on your skill requirements and candidate's
-          performance.
+          Recommendations are based on your skill requirements and
+          candidate&apos;s performance.
         </p>
       </div>
       <div className="mt-14 p-3 border-solid border-t-2">
         <ul className="space-y-2 ">
           {...candidateData.slice(3).map((candidate, index) => (
-            <li key={index} onClick={() => onCandidateClick(candidate.id)} className="text-gray-700 border-b-2 pb-1">
+            <li
+              key={index}
+              onClick={() => onCandidateClick(candidate.id)}
+              className="text-gray-700 border-b-2 pb-1"
+            >
               {candidate.name}
             </li>
           ))}
